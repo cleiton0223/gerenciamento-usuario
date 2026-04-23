ff817fbf8346a259743f2e03276a323686318425
Cleiton Bertoldi <cleitonbertoldi78@gmail.com>
2026-04-23 19:11:34 -0300

    feat: cria serviço de usuário com método de cadastro

---
+++ gerenciamento-usuario/src/main/java/api/gerenciamento_usuario/service/UsuarioService.java
@@ -0,0 +1,37 @@
+package api.gerenciamento_usuario.service;
+import api.gerenciamento_usuario.dto.UsuarioDto;
+import api.gerenciamento_usuario.entity.UsuarioEntity;
+import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UsuarioService {
+
+    private List<UsuarioEntity> arraylist = new ArrayList<>();
+
+    public boolean cadastrar(UsuarioDto usuarioDto) {
+
+        for (UsuarioEntity usuario : arraylist) {
+
+            if (usuario.getCpf().equals(usuarioDto.getCpf())) {
+                return false;
+            }
+
+            if (usuario.getLogin().equals(usuarioDto.getLogin())) {
+                return false;
+            }
+        }
+
+        UsuarioEntity novoUsuario = new UsuarioEntity();
+
+        novoUsuario.setLogin(usuarioDto.getLogin());
+        novoUsuario.setCpf(usuarioDto.getCpf());
+        novoUsuario.setNome(usuarioDto.getNome());
+        novoUsuario.setSenha(usuarioDto.getSenha());
+
+        arraylist.add(novoUsuario);
+
+        return true;
+    }
+}
